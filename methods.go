bc0f40dd7957b07125bb9d8ea6d12547f1899e89
Luca <shaohsiung@163.com>
2021-09-04 22:36:10 +0800

    methods: 指针接收者(需要修改它的接收者时, 请使用指针接收者)

--- methods.go
+++ methods.go
@@ -10,6 +10,9 @@ type Article struct {
 // Go方法就是带接收者的函数
 
 func (article Article) GetInfo() string {
+	// 这里把article的id修改为12, 只能影响这个方法的返回值, 不能影响到接收者的值.
+	// 需要修改接收者的值, 请使用指针接收者.
+	//article.id = 12
 	return fmt.Sprintf("id: %v, title: %s", article.id, article.title)
 }
 
@@ -17,4 +20,21 @@ type Integer int
 
 func (integer Integer) GetType() string {
 	return fmt.Sprintf("typo: %T", integer)
+}
+
+type Post struct {
+	Id    int
+	Title string
+}
+
+func (post *Post) UpdatePost(name string) {
+	if name == "" {
+		return
+	}
+
+	post.Title = name
+}
+
+func (post *Post) GetInfo() string {
+	return fmt.Sprintf("id: %v, title: %s", post.Id, post.Title)
 }
--- methods_test.go
+++ methods_test.go
@@ -11,4 +11,9 @@ func TestGetArticleInfo(t *testing.T) {
 
 	phoneNumber := Integer(13000000000)
 	assert.Equal(t, "typo: main.Integer", phoneNumber.GetType())
+
+	post := Post{Id: 10001, Title: "一片短文"}
+	assert.Equal(t, "id: 10001, title: 一片短文", post.GetInfo())
+	post.UpdatePost("一篇短文")
+	assert.Equal(t, "id: 10001, title: 一篇短文", post.GetInfo())
 }
