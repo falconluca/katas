81896c2fea19a49263d59c19dbfd9f51b39e5cad
Luca <shaohsiung@163.com>
2021-09-04 10:57:40 +0800

    var

--- variables.go
+++ variables.go
@@ -1,26 +1,31 @@
 package main
 
-//
-//import "fmt"
-//
-//// 变量 常量const
-//func main() {
-//	var name = "Luca"
-//	fmt.Printf("My name is %s.\n", name)
-//
-//	var page, total = 12, 20000
-//	fmt.Printf("page: %v, total: %v.\n", page, total)
-//
-//	// 声明并赋值
-//	responseString := "{msg: \"ok\",code: \"200\"}"
-//	fmt.Printf("HTTP Response Entity: %s", responseString)
-//
-//	// 先声明，再赋值
-//	var httpProxy string
-//	httpProxy = "127.0.0.1:8080"
-//	fmt.Printf("\nHTTP Proxy: %s", httpProxy)
-//
-//	// Const
-//	const BaiduHomePageUrl = "https://www.baidu.com"
-//	fmt.Println("\n" + BaiduHomePageUrl)
-//}
+import (
+	"errors"
+	"fmt"
+)
+
+func GetConstString() string {
+	const BaiduHomePageUrl = "https://www.baidu.com"
+	return BaiduHomePageUrl
+}
+
+func DefaultValueOfString() string {
+	var player string
+	return player
+}
+
+func ValueOfString() string {
+	//var player string = "Allen"
+	var player = "Allen"
+	return player
+}
+
+func FormatPlayerName(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("name can not be empty")
+	}
+	var player string
+	player = fmt.Sprintf("player name is %s", name)
+	return player, nil
+}
---
+++ variables_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestVariable(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Equal("https://www.baidu.com", GetConstString())
+	assert.Equal("", DefaultValueOfString())
+	assert.Equal("Allen", ValueOfString())
+
+	name, _ := FormatPlayerName("Luca")
+	assert.Equal("player name is Luca", name)
+
+	_, err := FormatPlayerName("")
+	assert.NotEmpty(err)
+	assert.Equal("name can not be empty", err.Error())
+}
