a1e361153bdd68ec7b968580be3cf0e6158fee5b
Luca <shaohsiung@163.com>
2021-09-05 10:16:54 +0800

    channel 设置buffer

--- concurrency/channel.go
+++ concurrency/channel.go
@@ -3,14 +3,19 @@ package concurrency
 import "fmt"
 
 func Channel() {
-	channel := make(chan string)
+	// https://stackoverflow.com/questions/11943841/what-is-channel-buffer-size
+	// buffer为2
+	channel := make(chan string, 2)
 
 	// goroutine向chan中发送消息, 与主线程通信
 	go func() {
 		channel <- "hello"
+		channel <- "golang"
 	}()
 
 	// 阻塞等待
-	result := <-channel
-	fmt.Println(result)
+	greeting := <-channel
+	name := <-channel
+	fmt.Println(greeting)
+	fmt.Println(name)
 }
