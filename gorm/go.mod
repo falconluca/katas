c5df13c2f9513850d9dabdf18d7fd16d258382d3
Luca <yangshaoxiong5545@gmail.com>
2021-10-23 22:42:54 +0800

    gorm

---
+++ gorm/go.mod
@@ -0,0 +1,18 @@
+module gorm
+
+go 1.17
+
+require (
+	gorm.io/driver/mysql v1.1.2
+	gorm.io/gorm v1.21.16
+)
+
+require (
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.2 // indirect
+	github.com/mattn/go-colorable v0.1.9 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+)
---
+++ gorm/hello.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"time"
+
+	"github.com/fatih/color"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type House struct {
+	// 包括哪些公共字段?
+	gorm.Model
+	Section string `gorm:"column:section"`
+	HouseId uint   `gorm:"column:house_id"`
+}
+
+var (
+	db *gorm.DB
+)
+
+func main() {
+	db = initDb()
+
+	// 更新表结构
+	//db.AutoMigrate(&House{})
+
+	logGreenStr("查询房源列表")
+	houses := getHousesBySection("万达")
+	for i, house := range *houses {
+		log.Printf("第%v套房源: %+v\n", i+1, house)
+	}
+
+	logGreenStr("创建房源")
+	newHouse := &House{HouseId: 7, Section: "万科"}
+	addNewHouse(newHouse)
+	log.Printf("新创建的房源: %+v\n", newHouse)
+
+	logGreenStr("删除房源(软删除: 因为有deletedAt字段)")
+	deleteHouseByHouseId(7)
+
+	logGreenStr("获取所有房源列表")
+	allHouses := findAllHouses()
+	for i, house := range *allHouses {
+		log.Printf("第%v套房源: %+v\n", i+1, house)
+	}
+
+	logGreenStr("查询房源")
+	house := getHouseByHouseId(12)
+	log.Printf("查询房源: %+v\n", house)
+
+	logGreenStr("更新房源")
+	house.Section = "万科"
+	updateHouse(house)
+
+	logGreenStr("分页获取房源")
+	housePage := getHousePage(0, 2)
+	for i, h := range housePage {
+		log.Printf("第%v套房源: %+v\n", i+1, *h)
+	}
+
+	// TODO 原生SQL
+	// TODO gorm钩子
+}
+
+func getHousePage(page int, size int) []*House { // 返回一个装满地址的数组...
+	houses := make([]*House, 0)
+	err := db.Where("house_id is not null").
+		Offset(page).
+		Limit(size).
+		Order("house_id desc").
+		Find(&houses).
+		Error
+	if err != nil {
+		log.Fatalf("获取房源分页数据失败! err: %v", err)
+	}
+	return houses
+}
+
+func updateHouse(house *House) {
+	if err := db.Save(house).Error; err != nil {
+		log.Fatalf("更新房源失败! err: %v", err)
+	}
+}
+
+func deleteHouseByHouseId(houseId uint) {
+	houseIdStr := strconv.Itoa(int(houseId))
+	// TODO 这个SQL好残疾 :) 只能这样写么?
+	err := db.Where("house_id = ?", houseIdStr).
+		//Unscoped(). // 永久删除
+		Delete(&House{}).
+		Error
+	if err != nil {
+		log.Fatalf("删除房源失败! err: %v", err)
+	}
+}
+
+func addNewHouse(house *House) {
+	if err := db.Create(house).Error; err != nil {
+		log.Fatalf("创建房源失败! err: %v", err)
+	}
+}
+
+func logGreenStr(str string) {
+	log.Println(color.GreenString(str))
+}
+
+func initDb() *gorm.DB {
+	dsn := fmt.Sprintf(`%s:%s@tcp(%s)/%s?charset=utf8&parseTime=%t&loc=%s`,
+		"root", "root", "127.0.0.1:3306", "test", true, "Local")
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect db")
+	}
+
+	// 配置连接池
+	// FIXME 可视化连接池的各种概念 mysql redis
+	datasource, err := db.DB()
+	if err != nil {
+		panic("create connections pool failed")
+	}
+	datasource.SetMaxIdleConns(100) // 最大空闲连接数
+	datasource.SetMaxOpenConns(100) // 最大连接数
+	datasource.SetConnMaxLifetime(time.Second * 10)
+	return db
+}
+
+func getHouseByHouseId(houseId uint) *House {
+	house := &House{}
+	houseIdStr := strconv.Itoa(int(houseId))
+	if err := db.Where("house_id = ?", houseIdStr).First(&house).Error; err != nil {
+		log.Fatalf("get house error: %v", err)
+	}
+	return house
+}
+
+func getHousesBySection(section string) *[]House { // 返回数组的地址...
+	houses := &[]House{}
+	if err := db.Where("section = ?", section).Find(houses).Error; err != nil {
+		log.Fatalf("get house error: %v", err)
+	}
+	return houses
+}
+
+func findAllHouses() *[]House {
+	houses := &[]House{}
+	if err := db.Find(houses).Error; err != nil {
+		log.Fatalf("find all houses error: %v", err)
+	}
+	return houses
+}
