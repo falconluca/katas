735e3c90c76a12e5e73c055e7e4f3f3e76db5d51
Luca <yangshaoxiong5545@gmail.com>
2021-10-24 11:15:54 +0800

    feat 博客api 注册文章模块路由

---
+++ blogapi/routers/api/v1/article.go
@@ -0,0 +1,28 @@
+package v1
+
+import "github.com/gin-gonic/gin"
+
+// GetArticle 获取文章
+func GetArticle(c *gin.Context) {
+
+}
+
+// GetArticles 获取文章列表
+func GetArticles(c *gin.Context) {
+
+}
+
+// AddArticle 添加文章
+func AddArticle(c *gin.Context) {
+
+}
+
+// EditArticle 编辑文章
+func EditArticle(c *gin.Context) {
+
+}
+
+// DeleteArticle 删除文章
+func DeleteArticle(c *gin.Context) {
+
+}
--- blogapi/routers/router.go
+++ blogapi/routers/router.go
@@ -25,6 +25,12 @@ func InitRouter() *gin.Engine {
 		apiv1.PUT("/tags/:id", v1.EditTag)
 		// 根据id删除标签
 		apiv1.DELETE("/tags/:id", v1.DeleteTag)
+
+		apiv1.GET("/articles", v1.GetArticles)
+		apiv1.GET("/articles/:id", v1.GetArticles)
+		apiv1.POST("/articles", v1.AddArticle)
+		apiv1.PUT("/articles/:id", v1.EditArticle)
+		apiv1.DELETE("/articles/:id", v1.DeleteArticle)
 	}
 
 	return r
