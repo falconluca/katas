27c25e7ebdd1bf4c7f5317735b32b4215ae16e47
Luca <yangshaoxiong5545@gmail.com>
2021-10-24 10:44:34 +0800

    feat 博客api 新增标签

--- blogapi/models/tag.go
+++ blogapi/models/tag.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"github.com/jinzhu/gorm"
+	"time"
+)
+
 type Tag struct {
 	Model
 
@@ -7,6 +12,19 @@ type Tag struct {
 	CreatedBy  string `json:"created_by"` // c.JSON时会自动转换格式
 	ModifiedBy string `json:"modified_by"`
 	State      int    `json:"state"`
+}
+
+// BeforeCreate gorm创建数据的回调方法
+func (tag *Tag) BeforeCreate(scope *gorm.Scope) error {
+	// TODO CreatedOn 或 created_on 都行
+	scope.SetColumn("created_on", time.Now().Unix())
+	return nil
+}
+
+// BeforeUpdate gorm更新数据的回调方法
+func (tag *Tag) BeforeUpdate(scope *gorm.Scope) error {
+	scope.SetColumn("modified_on", time.Now().Unix())
+	return nil
 }
 
 func GetTags(page int, size int, maps interface{}) (tags []Tag) {
@@ -23,4 +41,21 @@ func GetTagTotal(maps interface{}) (count int) {
 		Where(maps).
 		Count(&count)
 	return
+}
+
+// ExistTagByName 是否已经存在这个名称的标签
+func ExistTagByName(name string) bool {
+	var tag Tag
+	db.Select("id").Where("name = ?", name).First(&tag)
+	return tag.Id > 0
+}
+
+// AddTag 创建标签
+func AddTag(name string, state int, createdBy string) bool {
+	db.Create(&Tag{
+		Name:      name,
+		State:     state,
+		CreatedBy: createdBy,
+	})
+	return true
 }
--- blogapi/routers/api/v1/tag.go
+++ blogapi/routers/api/v1/tag.go
@@ -8,8 +8,11 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/unknwon/com"
 	"net/http"
+
+	"github.com/astaxie/beego/validation"
 )
 
+// curl http://localhost:8000/api/v1/tags
 func GetTags(c *gin.Context) {
 	// 组装数据访问参数
 	maps := make(map[string]interface{})
@@ -38,8 +41,33 @@ func GetTags(c *gin.Context) {
 	})
 }
 
+// curl -X POST "http://localhost:8000/api/v1/tags?name=Kubernetes&state=1&created_by=Luca"
 func AddTag(c *gin.Context) {
+	name := c.Query("name") // req.getParameter("name");
+	state := com.StrTo(c.DefaultQuery("state", "0")).MustInt()
+	createdBy := c.Query("created_by")
 
+	vaild := validation.Validation{}
+	vaild.Required(name, "name").Message("名称不能为空")
+	vaild.MaxSize(name, 12, "name").Message("名称最长为12个字符")
+	vaild.Required(createdBy, "created_by").Message("创建人不能为空")
+	vaild.MaxSize(createdBy, 8, "created_by").Message("创建人最长为8个字符")
+	vaild.Range(state, 0, 1, "state").Message("状态只允许0或1")
+
+	var code int
+	if vaild.HasErrors() {
+		code = e.INVALID_PARAMS
+	} else if models.ExistTagByName(name) {
+		code = e.ERROR_EXIST_TAG
+	} else {
+		models.AddTag(name, state, createdBy)
+		code = e.SUCCESS
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  e.GetMsg(code),
+		"data": make(map[string]string),
+	})
 }
 
 func EditTag(c *gin.Context) {
