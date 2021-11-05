42f73e405ec5b164fde41f7e235e48846d7bb83c
Luca <yangshaoxiong5545@gmail.com>
2021-11-05 16:40:01 +0800

    gorm 结构体地址的地址...

--- gorm/hello.go
+++ gorm/hello.go
@@ -29,55 +29,54 @@ func main() {
 	// 更新表结构
 	//db.AutoMigrate(&House{})
 
-	logGreenStr("查询房源列表")
-	houses := getHousesBySection("万达")
-	for i, house := range *houses {
-		log.Printf("第%v套房源: %+v\n", i+1, house)
+	//logGreenStr("查询房源列表")
+	//houses := getHousesBySection("万达")
+	//for i, house := range *houses {
+	//	log.Printf("第%v套房源: %+v\n", i+1, house)
+	//}
+	//
+	//logGreenStr("创建房源")
+	//newHouse := &House{HouseId: 7, Section: "万科"}
+	//addNewHouse(newHouse)
+	//log.Printf("新创建的房源: %+v\n", newHouse)
+	//
+	//logGreenStr("删除房源(软删除: 因为有deletedAt字段)")
+	//deleteHouseByHouseId(7)
+	//
+	//logGreenStr("获取所有房源列表")
+	//allHouses := findAllHouses()
+	//for i, house := range *allHouses {
+	//	log.Printf("第%v套房源: %+v\n", i+1, house)
+	//}
+
+	//logGreenStr("查询房源")
+	//house := getHouseByHouseId(12)
+	//log.Printf("查询房源: %+v\n", house)
+
+	//logGreenStr("更新房源")
+	//house.Section = "万科"
+	//updateHouse(house)
+
+	logGreenStr("分页获取房源")
+	housePage := getHousePage(0, 2)
+	for i, h := range housePage {
+		log.Printf("第%v套房源: %+v\n", i+1, *h)
 	}
 
-	/*
-		logGreenStr("创建房源")
-		newHouse := &House{HouseId: 7, Section: "万科"}
-		addNewHouse(newHouse)
-		log.Printf("新创建的房源: %+v\n", newHouse)
-
-		logGreenStr("删除房源(软删除: 因为有deletedAt字段)")
-		deleteHouseByHouseId(7)
-
-		logGreenStr("获取所有房源列表")
-		allHouses := findAllHouses()
-		for i, house := range *allHouses {
-			log.Printf("第%v套房源: %+v\n", i+1, house)
-		}
-
-		logGreenStr("查询房源")
-		house := getHouseByHouseId(12)
-		log.Printf("查询房源: %+v\n", house)
-
-		logGreenStr("更新房源")
-		house.Section = "万科"
-		updateHouse(house)
-
-		logGreenStr("分页获取房源")
-		housePage := getHousePage(0, 2)
-		for i, h := range housePage {
-			log.Printf("第%v套房源: %+v\n", i+1, *h)
-		}
-	*/
-
 	// 原生SQL
-	var h House
-	db.
-		Debug(). // 打印SQL
-		Raw("SELECT h1.section, h2.house_id, h2.updated_at FROM houses h1 "+
-			"left join houses h2 on h1.house_id = h2.house_id  WHERE h1.house_id = ?", 13).Scan(&h)
-	log.Printf("查询房源: %+v\n", h)
+	//var h House
+	//db.
+	//	Debug(). // 打印SQL
+	//	Raw("SELECT h1.section, h2.house_id, h2.updated_at FROM houses h1 "+
+	//		"left join houses h2 on h1.house_id = h2.house_id  WHERE h1.house_id = ?", 13).Scan(&h)
+	//log.Printf("查询房源: %+v\n", h)
 
 	// TODO gorm钩子
 }
 
 func getHousePage(page int, size int) []*House { // 返回一个装满地址的数组...
 	houses := make([]*House, 0)
+	// var houses []*House = make([]*House, 0)
 	err := db.Where("house_id is not null").
 		Offset(page).
 		Limit(size).
@@ -141,26 +140,37 @@ func initDb() *gorm.DB {
 }
 
 func getHouseByHouseId(houseId uint) *House {
-	house := &House{}
+	house := House{}
+	log.Printf("&House{}: %v", &house) // &{{0 0001-01-01 00:00:00 +0000 UTC 0001-01-01 00:00:00 +0000 UTC {0001-01-01 00:00:00 +0000 UTC false}}  0}
+
+	p := &house
+	log.Printf("&&House{}: %v", &p) // 0xc0000a2018
+	// 这是啥的地址呀??? 没整明白 TODO
+
 	houseIdStr := strconv.Itoa(int(houseId))
 	if err := db.Where("house_id = ?", houseIdStr).First(&house).Error; err != nil {
 		log.Fatalf("get house error: %v", err)
 	}
-	return house
+	return &house
 }
 
 func getHousesBySection(section string) *[]House { // 返回数组的地址...
-	houses := &[]House{}
-	if err := db.Where("section = ?", section).Find(houses).Error; err != nil {
+	var houses []House
+	if err := db.
+		//Select("house_id"). // 默认情况下, GORM 在查询时会选择所有的字段, 可以使用 Select 来指定您想要的字段
+		// 或者使用智能选择字段有点像Mybatis的resultType
+		Where("section = ?", section).
+		Find(&houses).Error; // 查看finisher_api.go
+	err != nil {
 		log.Fatalf("get house error: %v", err)
 	}
-	return houses
+	return &houses
 }
 
 func findAllHouses() *[]House {
-	houses := &[]House{}
-	if err := db.Find(houses).Error; err != nil {
+	var houses []House
+	if err := db.Find(&houses).Error; err != nil {
 		log.Fatalf("find all houses error: %v", err)
 	}
-	return houses
+	return &houses
 }
